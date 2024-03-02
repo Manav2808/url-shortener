2fad76211b8c1101b7b63cfec60a7e4ab9722996
Manav <manav18gadhiya@gmail.com>
2024-03-02 23:42:36 +0530

    Project Completed

--- controllers/url.js
+++ controllers/url.js
@@ -4,7 +4,14 @@ import URL from "../models/url.js";
 async function generateNewShortUrl(req, res) {
     const body = req.body;
 
-    if (!body.url) return res.status(400).json({ error: "Please enter a URL" });
+    if (!body.url) {
+        return res.json({ error: "No URL entered" });
+    }
+
+    const url = await URL.findOne({ url: body.url });
+    if (url) {
+        return res.json({ id: url.shortUrl });
+    }
 
     const shortUrl = nanoid(7);
     await URL.create({
--- index.js
+++ index.js
@@ -2,21 +2,34 @@ import express from "express";
 import mongoose from "mongoose";
 import urlRouter from "./routes/url.js";
 import URL from "./models/url.js";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 const MONGODB_URL = "mongodb://127.0.0.1:27017/short-urls"
 
-app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'))
 
+app.use(express.json());
 app.use('/url', urlRouter);
-app.use('/:shortUrl', async (req, res) => {
+
+app.get('/', async (req, res) => {
+    return res.render('home');
+});
+
+app.get('/:shortUrl', async (req, res) => {
     const shortUrl = req.params.shortUrl;
 
     const url = await URL.findOne({ shortUrl: shortUrl });
 
-    res.redirect(url.url);
-})
+    if (url) {
+        return res.redirect(url.url);
+    }
+
+    return res.render('error');
+
+});
 
 try {
     mongoose.connect(MONGODB_URL)
